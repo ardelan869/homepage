010e69ad2c6b41e8fbf269b2a39297cd08871090
Ardelan <ardelanyamanel@outlook.de>
2024-05-27 06:46:37 +0200

    refactor(header): Removed Github, since its already in the footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,20 @@
-import { Github } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+'use client';
 
 import Link from 'next/link';
 
+import config from '@/config';
 import NavBar from '@/components/NavBar';
 import ColorSchemeSwitcher from '@/components/ColorSchemeSwitcher';
-import config from '@/config';
 
 export default function Header() {
 	return (
-		<header className="sticky top-0 z-10 top animate-ttb backdrop-blur-xl">
-			<div className="container py-2 border-b lg-sm:grid lg-sm:grid-cols-3 lg-sm:grid-rows-1 flex justify-between items-center">
+		<header className="sticky top-0 z-10 top animate-ttb backdrop-blur-xl bg-foreground/5 transition-colors">
+			<div className="container py-2 border-b grid grid-cols-3 grid-rows-1 items-center">
 				<Link href="/">
 					<h1 className="font-mono text-lg font-bold">{`${config.personal.firstName} ${config.personal.lastName[0]}.`}</h1>
 				</Link>
 				<NavBar />
 				<div className="flex items-center gap-2 justify-self-end">
-					<Link
-						href={`https://github.com/${config.github.username}`}
-						target="_blank"
-					>
-						<Button variant="ghost" className="p-2 w-9 h-9">
-							<Github />
-						</Button>
-					</Link>
 					<ColorSchemeSwitcher />
 				</div>
 			</div>
